970963e383937ec666b8b6028737900deee2b833
Lília Maria <liliamaria.lys@gmail.com>
2023-05-26 15:38:33 -0300

    criei aluno, funcionario, passageiro e editei pessoa

--- resources/pessoa.py
+++ resources/pessoa.py
@@ -4,6 +4,9 @@ from flask_restful import Resource, reqparse, marshal_with, marshal
 from model.pessoa import *
 from model.endereco import *
 from model.message import *
+from model.aluno import *
+from model.passageiro import *
+from model.funcionario import *
 from helpers.database import db
 # from helpers.base_logger import logger
 
