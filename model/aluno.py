1d188c15b77612c9f701857cedaf2788c162221a
JoalisonM <joalisonmatheus10@gmail.com>
2023-05-28 20:40:28 -0300

    created person and student endpoints

--- model/aluno.py
+++ model/aluno.py
@@ -5,11 +5,10 @@ from model.pessoa import Pessoa
 aluno_fields = {
   'id': fields.Integer,
   'nome': fields.String,
-  'nascimento': fields.DateTime,
+  'nascimento': fields.String,
   'email': fields.String,
   'telefone': fields.String,
   'senha' : fields.String,
-  'idEndereco': fields.Integer,
   'matricula' : fields.String,
   'curso' : fields.String,
   'turno' : fields.String,
@@ -20,17 +19,19 @@ class Aluno(Pessoa):
   __tablename__ = "aluno"
 
   idPessoa = db.Column(db.Integer ,db.ForeignKey("pessoa.id"), primary_key=True)
-  matricula = db.Column(db.String, unique=True, nullable=False)
-  curso = db.Column(db.String, nullable=False)
-  turno = db.Column(db.String, nullable=False)
-  idInstituicaoEnsino =  db.Column(db.Integer, db.ForeignKey('instituicaoEnsino.id'))
+  matricula = db.Column(db.String, unique=True, nullable=True)
+  curso = db.Column(db.String, nullable=True)
+  turno = db.Column(db.String, nullable=True)
+  idInstituicaoEnsino =  db.Column(
+    db.Integer, db.ForeignKey('instituicaoEnsino.id'), nullable=True
+  )
 
   passageiro = db.relationship("Passageiro", uselist=False, backref="aluno")
 
   __mapper_args__ = {"polymorphic_identity": "aluno"}
 
-  def __init__(self, nome, email, nascimento, telefone, senha, idEndereco, matricula, curso, turno, idInstituicaoEnsino):
-    super().__init__(nome, email, nascimento, telefone, senha, idEndereco)
+  def __init__(self, nome, email, nascimento, telefone, senha, matricula, curso, turno, idInstituicaoEnsino):
+    super().__init__(nome, email, nascimento, telefone, senha)
     self.curso = curso
     self.turno = turno
     self.matricula = matricula
