de6acdc6a91e94b82aae3d3b884e3497196daeea
JoalisonM <joalisonmatheus10@gmail.com>
2023-05-28 20:44:19 -0300

    adjusted port

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ api.add_resource(PessoaById, '/pessoas/<int:id>')
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=5000)
