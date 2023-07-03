9032fae8a4adffe0c0e56c2637628ae7798f3448
JoalisonM <joalisonmatheus10@gmail.com>
2023-07-03 17:50:25 -0300

    ajuste de conflitos

---
+++ migrations/versions/3334a1338e1e_mergin_two_heads.py
@@ -0,0 +1,24 @@
+"""mergin two heads
+
+Revision ID: 3334a1338e1e
+Revises: 8a76e59fd3bc, cab188b640a1
+Create Date: 2023-07-03 16:59:52.950168
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3334a1338e1e'
+down_revision = ('8a76e59fd3bc', 'cab188b640a1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
